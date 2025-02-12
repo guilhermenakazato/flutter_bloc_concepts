b162ed040f661f753d93a8509168d13b3d1c708b
guilhermenakazato <guilherme.fernandes23903@gmail.com>
2025-02-12 01:00:42 -0400

    [FEAT] Adding tests

--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -8,4 +8,17 @@ class CounterState {
     required this.counterValue,
     this.wasIncremented,
   });
+
+  @override
+  bool operator ==(Object other) {
+    if (other is CounterState) {
+      return counterValue == other.counterValue &&
+          wasIncremented == other.wasIncremented;
+    }
+
+    return false;
+  }
+  
+  @override
+  int get hashCode => Object.hash(counterValue, wasIncremented);
 }
